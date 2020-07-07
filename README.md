ff2939c1afcf08a527501b5e3aa0011e97e43c8e
DCR430 <daniel.c.reyes30@gmail.com>
2020-07-07 13:40:38 -0400

    attempting to connect user and post

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 Rails C => User.first => password:[FILTERED] #What does this mean?!?
 
+Posts Index Page => Posts author to connect to author show page??
 
 
 
 
+
+User Followers list? @users
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,33 @@ class PostsController < ApplicationController
 
     def index 
         @posts = Post.all
+  
     end
 
     def show
-
-        @post = Post.find_by(params[:id])
-        # byebug
+        @post = Post.find(params[:id])
+        @user = User.find(params[:id])
     end
+
+    def new
+        @post = Post.new
+
+    end
+
+    def create 
+        @post = Post.create(post_params)
+        redirect_to posts_path
+    end
+
+
+
+
+private
+
+    def post_params
+        params.require(:post).permit(:post_title, :post_body, :post_description, :user_id)
+    end
+
+
 end
+
