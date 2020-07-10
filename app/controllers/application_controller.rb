7b61b7243d90089b2d906c4175373fff892f2ba3
DCR430 <daniel.c.reyes30@gmail.com>
2020-07-10 12:12:17 -0400

    demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,10 @@ class ApplicationController < ActionController::Base
     def auth_user
 
         # redirect_to posts_path unless set_user
-        redirect_to new_user_path unless set_user
-    
+        # byebug    #session.id "a92786af31e3b93b45e510b618a7bedd"
+        redirect_to new_post_path unless set_user
+          
     end
 
 end
+
